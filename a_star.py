8c2658bd3e3f54b49b0ab40aed7be6a5f8e406c9
santhosh1996 <santhoshkumara1996@gmail.com>
2020-10-27 16:20:12 +0530

    Implemented data structures (Hash to chk node exists, priority Q to choose parent)

--- a_star.py
+++ a_star.py
@@ -12,7 +12,21 @@ class Node:
         self.action = action
         self.parent = parent
         self.visited = False
+        self.act = self.config.takeaction(self.action)
+        self.hash = self.getHash()
 
+    def getHash(self,):
+        h = ''
+        for row in self.config.box:
+            for ele in row:
+                h += ele
+        return h
+
+    # for heap comparing
+    def __lt__(self, other):
+        return self.f < other.f
+        
+nodeH = {}
 n = int(input("Enter Board Size: "))
 Q = []
 box = np.Box(n)
@@ -45,66 +59,45 @@ def get_cost(box):
 
     return cost
 
-Q.append(Node(box,0,get_cost(box.box), None, None))
+q = []
+heapq.heappush(q, Node(box,0,get_cost(box.box), None, None))
 
 def check_node_exists(node):
-    for inode in Q:
-        flag = False
-        for i in range(node.config.size):
-            for j in range(node.config.size):
-                if(inode.config.box[i][j] != node.config.box[i][j]):
-                    flag = True
-                    break
-            if(flag): break
-        if(not flag):
-            return False
-    return True
-
-to_visit = [Q[0]]
-visited_nodes = []
+    if(node.hash in nodeH):
+        return True
+    return False
+        
 count = 0
 solution = None
 box_action = [np.Box.left, np.Box.right, np.Box.up, np.Box.down]
 box.display()
+nodeH[q[0].hash] = 1
+
 while(True):
     flag = False
-    for node in to_visit:
-        parent = node
-
-        for action in box_action:
-            new_node = Node(parent.config, parent.g + 1, get_cost(parent.config.box), action, parent)
-            act = new_node.config.takeaction(action)
-            if(check_node_exists(new_node) and act):
-                Q.append(new_node)
-                if(get_cost(new_node.config.box) == 0):
-                    solution = new_node
-                    flag = True
-                    break
-
-        parent.visited = True
-        count += 1
-    # populate to_visit nodes ( nodes with min f will be choosen to visit )
-    if(flag == False):
-        to_visit = []
-        mn_f = None
-        for node in Q:
-            if(node.visited != True):
-                if(mn_f is None):
-                    mn_f = node.f
-                elif(mn_f > node.f):
-                    mn_f = node.f
-        if(mn_f == None):
-            print("all are visited")
-            input("Hmm...")
-        for node in Q:
-            if(node.f == mn_f and node.visited != True):
-                to_visit.append(node)
-        
-        print("Items to explore in this round {}".format(len(to_visit)),end='\r')
-
+    ####
+    if(q):
+        parent = heapq.heappop(q)
     else:
+        print("Solution not found")
+        break
+    for action in box_action:
+        new_node = Node(parent.config, parent.g + 1, get_cost(parent.config.box), action, parent)
+        if(not check_node_exists(new_node) and new_node.act):
+            heapq.heappush(q, new_node)
+            nodeH[new_node.hash] = 1
+            if(get_cost(new_node.config.box) == 0):
+                solution = new_node
+                flag = True
+                break
+
+    parent.visited = True
+    count += 1
+
+    print("Total number of nodes explored {}".format(count),end='\r')
+    if(flag):
         actions = []
-        print("Solved it ")
+        print("\nSolved it ")
         n_node = solution
         while(n_node.parent != None):
             actions.append(n_node.action)
@@ -119,5 +112,5 @@ while(True):
             if(i != len(actions)-1):
                 print("\033[A"*r)
         print("Number of moves made: {}".format(len(actions)))
-        print("Numbers of nodes explores: {}".format(count))
         exit()
+    ####
